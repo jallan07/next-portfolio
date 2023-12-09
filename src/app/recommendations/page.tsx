7801187a8746569e77ff4e8d9f33cfb101943cf3
Josh Allan <allan.josh07@gmail.com>
2023-12-09 14:49:53 -0500

    update footer

--- src/app/recommendations/page.tsx
+++ src/app/recommendations/page.tsx
@@ -29,11 +29,11 @@ export default async function Recommendations() {
                 </p>
                 <div className='flex gap-3'>
                     <Button href="#dev" color="gray" className='mt-8 text-sm'>
-                        View Recommendations as a Developer
+                        View Developer Recs
                         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
                     </Button>
                     <Button href="https://linkedin.com/in/joshuamallan/details/recommendations/" color="gray" className='mt-8 text-sm' variant='secondary'>
-                        View All Recommendations
+                        View All Recs
                         <NewSiteIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
                     </Button>
                 </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,6 +29,7 @@ export function Footer() {
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/">Home</NavLink>
                 <NavLink href="/about">About</NavLink>
+                <NavLink href="/recommendations">Recommendations</NavLink>
                 {/* <NavLink href="/projects">Projects</NavLink>
                 <NavLink href="/speaking">Speaking</NavLink>
                 <NavLink href="/uses">Uses</NavLink> */}
