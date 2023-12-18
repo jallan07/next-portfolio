919c0dc41989c514736647c24c1cb0ab3df11fc9
Josh Allan <allan.josh07@gmail.com>
2023-12-18 13:51:03 -0500

    move api dir up one level

--- src/server/getRecommendations.ts
+++ src/server/getRecommendations.ts
@@ -2,10 +2,7 @@ export const getRecommendations = async () => {
   const result = await fetch(`${process.env.API_URL}/api/recommendations`, {
     method: 'GET',
   })
-  console.log(
-    '🚀 ~ file: getRecommendations.ts:4 ~ getRecommendations ~ `${process.env.API_URL}/api/recommendations`:',
-    `${process.env.API_URL}/api/recommendations`,
-  )
+
   if (result.ok) {
     return result.json()
   }
