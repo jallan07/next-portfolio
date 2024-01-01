3547042f127a4803085559654736e08596f567df
Josh Allan <allan.josh07@gmail.com>
2024-01-01 14:40:17 -0500

    fix grammar

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -100,7 +100,7 @@ export default function ContactForm() {
             <Textarea
               className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
               id="message"
-              placeholder="Leave a message at the tone....beeeeep."
+              placeholder="Leave a message after the tone....beeeeep."
               required
               rows={4}
             />
