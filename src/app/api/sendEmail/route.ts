fbb5428b48f4847d03bac141d38f908b92f58f16
Josh Allan <allan.josh07@gmail.com>
2024-01-01 14:15:59 -0500

    fix linting error

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,10 +1,11 @@
 import { EmailTemplate } from '@/components/EmailTemplate'
+import { NextResponse } from 'next/server'
 import { Resend } from 'resend'
 
 const toAddress = process.env.EMAIL_ADDRESS!
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST(req: Request): Promise<Response> {
+export async function POST(req: Request) {
   const url = new URL(req.url)
   let params = new URLSearchParams(url.search)
 
@@ -31,8 +32,8 @@ export async function POST(req: Request): Promise<Response> {
       text: message,
     })
 
-    return Response.json({ data })
+    return NextResponse.json({ data })
   } catch (error) {
-    return Response.json({ error })
+    return NextResponse.json({ error })
   }
 }
