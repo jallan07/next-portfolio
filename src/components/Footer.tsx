b885cdbf895dda6e39216dbdbf925b09a8b93116
Josh Allan <allan.josh07@gmail.com>
2024-01-03 09:41:32 -0500

    update header and footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,10 +27,10 @@ export function Footer() {
           <ContainerInner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                <NavLink href="/">Home</NavLink>
                 <NavLink href="/experience">Experience</NavLink>
                 <NavLink href="/recommendations">Recommendations</NavLink>
                 <NavLink href="/about">About</NavLink>
+                <NavLink href="/contact">Contact</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Josh Allan | All rights
